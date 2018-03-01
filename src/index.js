4206675969b94ec34e6c510b6de055dfd36965cb
VladSemenik <36641413+VladSemenik@users.noreply.github.com>
2018-03-01 18:21:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var result =0;
+    for(var i=5; i<=number; i*=5){
+      result += Math.floor(number/i);
+  }
+    return result;
 }
